f09505a58c13bcff0a78c84dbde1722bf3339a96
Nandhan-KA <nandhan2003alamelu@gmail.com>
2025-02-07 00:52:07 +0530

    new

--- source/conf.py
+++ source/conf.py
@@ -25,7 +25,6 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']  
-html_css_files = ['custom.css']
 
 # Path to the logo image
 html_logo = '_static/Logo.png'  # Ensure the logo image is in the source/_static directory
